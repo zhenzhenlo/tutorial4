06d23209eafd3a3b098a3f2cc995e1153a207a9b
Ivory <qiaofei98@gmail.com>
2024-08-18 15:40:06 +1200

    Implement subtract method to pass the test

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -2,13 +2,11 @@ package org.example;
 
 public class Calc {
 
-    public int add(int x, int y)
-    {
-        return x+y;
+    public int add(int x, int y) {
+        return x + y;
     }
 
-    public int subtract(int x, int y)
-    {
-        return x*y;
+    public int subtract(int x, int y) {
+        return x - y;
     }
 }
